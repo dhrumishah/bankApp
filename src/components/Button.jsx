07bb5d6a716eff9256c8bfb7c8c028a9eb54b1cc
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-11-16 21:46:37 +0530

    added phenomenal feature section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`py-[0.5px] px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
   >
     Get Started
   </button>
