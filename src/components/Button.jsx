8180c2c97af872e05281e87460e1e792262b253a
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-11-17 14:16:29 +0530

    CardDeal added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles} transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles} rounded-[10px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
   >
     Get Started
   </button>
